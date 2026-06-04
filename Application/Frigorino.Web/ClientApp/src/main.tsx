ee025d30683b4289bc9220d50d346257aaa1b336
Dennis <dennis.kaminski95@gmail.com>
2026-06-04 09:50:35 +0200

    feat(toasts): show close button so toasts can be dismissed

--- Application/Frigorino.Web/ClientApp/src/main.tsx
+++ Application/Frigorino.Web/ClientApp/src/main.tsx
@@ -84,6 +84,7 @@ createRoot(document.getElementById("root")!).render(
                     <RouterProvider router={router} />
                 </Suspense>
                 <Toaster
+                    closeButton
                     toastOptions={{
                         classNames: { actionButton: "undo-action-button" },
                     }}
